58b4ffe309498d2b9e87c6f67438029f3e8369b2
tckz <at.tckz@gmail.com>
2023-10-02 21:26:59 +0900

    go1.21, aws sdk v2

--- cmd/send-raw-email/main.go
+++ cmd/send-raw-email/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -9,11 +11,14 @@ import (
 	"os"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
+	"github.com/goccy/go-yaml"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	"github.com/samber/lo"
 	"gopkg.in/gomail.v2"
 )
 
@@ -43,6 +48,7 @@ func main() {
 	optBody := flag.String("body", "", "/path/to/mail-body.txt")
 	optFrom := flag.String("from", "", "From address")
 	optSubject := flag.String("subject", "", "Subject")
+	optContentType := flag.String("content-type", "text/plain", "Content-Type")
 	optConfigurationSet := flag.String("configuration-set", "", "SES configuration set")
 	flag.Parse()
 
@@ -61,12 +67,14 @@ func main() {
 
 	m := gomail.NewMessage()
 	fromDomain := ""
+	var fromAddr *mail.Address
 	{
 		addr, err := mail.ParseAddress(*optFrom)
 		if err != nil {
 			log.Fatalf("*** ParseAddress(%s): %v", *optFrom, err)
 		}
-		m.SetHeader("From", *optFrom)
+		m.SetAddressHeader("From", addr.Address, addr.Name)
+		fromAddr = addr
 
 		// ParseAddressを通過しているので、ドメイン名部分が存在する
 		i := strings.LastIndexByte(addr.Address, '@')
@@ -76,71 +84,104 @@ func main() {
 		m.SetHeader("Subject", *optSubject)
 	}
 
-	var dests []*string
-	appendDest := func(addrs []string) error {
-		for _, to := range optTo {
+	var tos []*mail.Address
+	var ccs []*mail.Address
+	var bccs []*mail.Address
+	appendDest := func(addrs []string, dests *[]*mail.Address) error {
+		for _, to := range addrs {
 			addr, err := mail.ParseAddress(to)
 			if err != nil {
 				return fmt.Errorf("mail.ParseAddress(%s): %v", to, err)
 			}
-			dests = append(dests, aws.String(addr.Address))
+			*dests = append(*dests, addr)
 		}
 		return nil
 	}
-	appendDestAndHeader := func(addrs []string, field string) error {
-		err := appendDest(addrs)
+	appendDestAndHeader := func(addrs []string, field string, dests *[]*mail.Address) error {
+		err := appendDest(addrs, dests)
 		if err != nil {
 			return err
 		}
 		if len(addrs) > 0 {
+			addrs := lo.Map(*dests, func(addr *mail.Address, _ int) string { return m.FormatAddress(addr.Address, addr.Name) })
 			m.SetHeader(field, addrs...)
 		}
 		return nil
 	}
-	if err := appendDestAndHeader(optTo, "To"); err != nil {
+	if err := appendDestAndHeader(optTo, "To", &tos); err != nil {
 		log.Fatalf("*** To: %v", err)
 	}
-	if err := appendDestAndHeader(optCC, "Cc"); err != nil {
+	if err := appendDestAndHeader(optCC, "Cc", &ccs); err != nil {
 		log.Fatalf("*** Cc: %v", err)
 	}
-	if err := appendDest(optBCC); err != nil {
+	if err := appendDest(optBCC, &bccs); err != nil {
 		log.Fatalf("*** Bcc: %v", err)
 	}
 
 	msgid := uuid.New().String() + "@" + fromDomain
+	// SESによって上書きされ、この値は使われない
 	m.SetHeader("Message-ID", "<"+msgid+">")
 
-	m.SetBody("text/plain", string(b))
+	m.SetBody(*optContentType, string(b))
 
 	sb := &bytes.Buffer{}
 	m.WriteTo(sb)
 	os.Stderr.Write(sb.Bytes())
 
-	ac := aws.NewConfig()
-	sess := session.Must(session.NewSessionWithOptions(
-		session.Options{
-			Config:            *ac,
-			SharedConfigState: session.SharedConfigEnable,
-		}))
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(os.Getenv("AWS_REGION")))
+	if err != nil {
+		log.Fatalf("config.LoadDefaultConfig: %v", err)
+	}
 
-	cl := ses.New(sess)
-	in := &ses.SendRawEmailInput{
-		Destinations:  dests,
-		FromArn:       nil,
-		RawMessage:    &ses.RawMessage{Data: sb.Bytes()},
-		ReturnPathArn: nil,
-		Source:        nil,
-		SourceArn:     nil,
+	cl := sesv2.NewFromConfig(cfg)
+	in := &sesv2.SendEmailInput{
+		Content: &types.EmailContent{
+			Raw: &types.RawMessage{Data: sb.Bytes()},
+		},
+		Destination: &types.Destination{
+			BccAddresses: lo.Map(bccs, func(addr *mail.Address, _ int) string { return addr.Address }),
+			CcAddresses:  lo.Map(ccs, func(addr *mail.Address, _ int) string { return addr.Address }),
+			ToAddresses:  lo.Map(tos, func(addr *mail.Address, _ int) string { return addr.Address }),
+		},
+		FromEmailAddress: aws.String(fromAddr.Address),
 	}
 
 	if *optConfigurationSet != "" {
 		in.ConfigurationSetName = optConfigurationSet
 	}
 
-	out, err := cl.SendRawEmail(in)
+	out, err := cl.SendEmail(ctx, in)
 	if err != nil {
 		log.Fatalf("*** SendEmail: %v", err)
 	}
 
-	fmt.Fprintf(os.Stderr, "%s\n", out)
+	{
+		b, err := marshalYaml(out)
+		if err != nil {
+			log.Fatalf("*** marshalYaml: %v", err)
+		}
+
+		fmt.Fprintf(os.Stderr, "---\n%s\n", string(b))
+	}
+}
+
+// marshalYaml yaml tagのないstructをyaml.Marshalするとkeyがlowercaseになってしまうので一回JSONでmap[string]interface{}に変換してからyaml.Marshalすると大文字小文字を維持できる
+func marshalYaml(v any) ([]byte, error) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return nil, fmt.Errorf("json.Marshal: %w", err)
+	}
+	dec := json.NewDecoder(bytes.NewReader(b))
+	dec.UseNumber()
+	var o any
+	err = dec.Decode(&o)
+	if err != nil {
+		return nil, fmt.Errorf("dec.Decode: %w", err)
+	}
+	b, err = yaml.MarshalWithOptions(o, yaml.UseLiteralStyleIfMultiline(true))
+	if err != nil {
+		return nil, fmt.Errorf("yaml.MarshalWithOptions: %w", err)
+	}
+	return b, nil
 }
--- go.mod
+++ go.mod
@@ -1,15 +1,34 @@
 module github.com/tckz/go-ses
 
-go 1.19
+go 1.21
 
 require (
-	github.com/aws/aws-sdk-go v1.44.77
-	github.com/google/uuid v1.3.0
-	github.com/joho/godotenv v1.4.0
+	github.com/aws/aws-sdk-go-v2 v1.21.0
+	github.com/aws/aws-sdk-go-v2/config v1.18.42
+	github.com/aws/aws-sdk-go-v2/service/sesv2 v1.20.1
+	github.com/goccy/go-yaml v1.11.2
+	github.com/google/uuid v1.3.1
+	github.com/joho/godotenv v1.5.1
+	github.com/samber/lo v1.38.1
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.40 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.41 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.43 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.35 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.14.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.17.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.22.0 // indirect
+	github.com/aws/smithy-go v1.14.2 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
